e27e5c39c06e0feea3d516bd8ef61d211d0f75d7
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-05-01 14:32:47 +0530

    Added Model Classes(V1.3)

--- src/main/java/com/example/eventmanagement/Controller/ParticipantController.java
+++ src/main/java/com/example/eventmanagement/Controller/ParticipantController.java
@@ -1,11 +1,31 @@
 package com.example.eventmanagement.Controller;
 
+import com.example.eventmanagement.Models.Participant;
+import com.example.eventmanagement.Models.Registration;
+import com.example.eventmanagement.Services.ParticipantService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/participant")
 public class ParticipantController {
+    private ParticipantService participantService;
+    ParticipantController(ParticipantService participantService) {
+        this.participantService = participantService;
+    }
 
+    @PostMapping("")
+    public Participant createParticipant(@RequestBody Participant participant) {
+        return null;
+    }
+
+    @PutMapping("")
+    public Participant deleteParticipant(@RequestBody Participant participant) {
+        return null;
+    }
+
+    @PostMapping ("/register")
+    public Registration register(@RequestBody Registration registration) {
+        return null;
+    }
 }
