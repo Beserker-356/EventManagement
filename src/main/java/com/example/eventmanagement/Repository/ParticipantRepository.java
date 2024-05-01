31e2b588c3a0f94f9f503a1d831053d48ad3a203
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-05-01 16:01:59 +0530

    Added Model Classes(V1.4)

---
+++ src/main/java/com/example/eventmanagement/Repository/ParticipantRepository.java
@@ -0,0 +1,9 @@
+package com.example.eventmanagement.Repository;
+
+import com.example.eventmanagement.Models.Participant;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipantRepository{
+
+}
