17fd080bd0a80b0e1af7d48f79c8acc4a29b3c80
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-05-01 23:11:15 +0530

    modified Participant Model

--- src/main/java/com/example/eventmanagement/Repositories/RegistrationRepository.java
+++ src/main/java/com/example/eventmanagement/Repositories/RegistrationRepository.java
@@ -3,12 +3,13 @@ package com.example.eventmanagement.Repositories;
 import com.example.eventmanagement.Models.Organiser;
 import com.example.eventmanagement.Models.Participant;
 import com.example.eventmanagement.Models.Registration;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RegistrationRepository {
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
     Registration save(Registration registration);
     void deleteById(Long id);
     Optional<Registration> findById(Long id);
--- src/main/java/com/example/eventmanagement/Repositories/VenueRepository.java
+++ src/main/java/com/example/eventmanagement/Repositories/VenueRepository.java
@@ -1,14 +1,14 @@
 package com.example.eventmanagement.Repositories;
 
 import com.example.eventmanagement.Models.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-
-public interface VenueRepository {
+public interface VenueRepository extends JpaRepository<Venue, Long> {
     Venue save(Venue venue);
 
     List<Venue> findAll();
