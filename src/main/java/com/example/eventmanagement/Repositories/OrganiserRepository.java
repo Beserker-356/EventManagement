f46b2949b7cd9f59ccae999dc211b52f0fdf6d32
Krishna Patidar <113329860+krishna-p060@users.noreply.github.com>
2024-05-01 16:31:37 +0530

    organiser CRUD done

--- src/main/java/com/example/eventmanagement/Repositories/OrganiserRepository.java
+++ src/main/java/com/example/eventmanagement/Repositories/OrganiserRepository.java
@@ -4,9 +4,18 @@ import com.example.eventmanagement.Models.Organiser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrganiserRepository extends JpaRepository<Organiser, Long>{
     Organiser save(Organiser organiser);
+
+    void deleteById(Long organiserId);
+
+    Optional<Organiser> findById(Long organiserId);
+
+    List<Organiser> findAll();
 
 
 }
